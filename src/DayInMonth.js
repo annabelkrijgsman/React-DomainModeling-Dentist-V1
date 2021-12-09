7f831e41c29fe6dfab5c96df82547b82ee1f1148
Annabel Krijgsman <annabelkrijgsman@gmail.com>
2021-12-09 13:09:03 +0100

    code clean-up

--- src/DayInMonth.js
+++ src/DayInMonth.js
@@ -5,5 +5,6 @@ export default ({ appointments }) => {
   const appointmentsJSX = appointments.map(({ time, patient }, index) => (
     <AppointmentInMonth time={time} patient={patient} key={index} />
   ))
+
   return <div className="day">{appointmentsJSX}</div>
 }
